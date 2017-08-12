ec98a4f8fccfca135dc7b1d887db2fa1959da451
Jude Kikuyu <jkikuyu@gmail.com>
2017-08-12 09:48:04 +0300

    add selection ability for different users

--- index.php
+++ index.php
@@ -1,26 +1,45 @@
 <?php
 	session_start();
 	require_once "classes/classesAutoload.php";
-	if (isset($_POST["username"])){	
-		$success = $objLogin->checkValidUser($MYSQL);
-		if ($success){
 
-			$objLayout->head();
-			$objLayout->body_open();
-			$objLayout->list_container();
+	if (isset($_POST["username"])){	
+		$success= $objLogin->checkValidUser($MYSQL);
+		if ($success){
+				$objLayout->head();
+				$objLayout->body_open();
+
+			switch($_SESSION["userId"]){
+				case 1:
+				$objLayout->admin_container();
+				break;
+				case 2:
+
+			}  
+			$objLayout->footer();			
 			//$objLayout->user_lists();
-			$objLayout->footer();
+			
 		}
 		else{
 			$objLogin->show_login();
 			//$objLayout->errorMessage("error");
 		}
 	}
+	elseif (isset($_SESSION["username"])){
+		$objLayout->head();
+		$objLayout->body_open();
+
+		switch ($_SESSION["username"]){
+			case 'admin':
+			$objLayout->admin_container();
+			break;
+
+		}
+	}
 	else{
 
 		$objLogin->show_login();
-
-
 	}
+	
+
 ?>
  
--- lang/en.php
+++ lang/en.php
@@ -1,9 +1,19 @@
 <?php
 
-$lang["pagetitle"] = "Welcome to Habari Leo";
+$lang["title"] = "Welcome to Habari Leo";
+$lang["personal_info"] = "Personal Information";
 $lang["username"] = "Username";
+$lang["fullname"] = "Full Name";
+
 $lang["password"] = "Password";
+$lang["email"] = "Email";
+$lang["phonenumber"] = "Phone Number";
+$lang["profile_image"]="Profile Image";
+$lang["returnbutton"] = "Return";
+$lang["updatedetailsbutton"] = "Update Record";
+$lang["savedetailsbutton"] = "Save Details";
 $lang["savedetails"] = "Save Details";
+$lang["movetoadmin"] = "Move this area to an administrator's page";
 $lang["copyright"] = "Copyright &copy; " . date("Y") . " - BBIT3104";
 $lang["table_name"] = $_SESSION["table_name"];
 $lang["group_name"] = ucwords(strtolower(str_replace( '_' , ' ' , $lang["table_name"] )));
